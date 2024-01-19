99ffa055ce17f61784cd8fdc95986fec7bef85cb
totato4 <v4.97@list.ru>
2024-01-19 11:15:04 +0300

    main

---
+++ components/SaveFormBtn.tsx
@@ -0,0 +1,43 @@
+import React, { useTransition } from "react";
+import { Button } from "./ui/button";
+import { HiSaveAs } from "react-icons/hi";
+import useDesigner from "./hooks/useDesigner";
+import { UpdateFormContent } from "@/actions/form";
+import { toast } from "./ui/use-toast";
+import { FaSpinner } from "react-icons/fa";
+
+function SaveFormBtn({ id }: { id: number }) {
+  const { elements } = useDesigner();
+  const [loading, startTransition] = useTransition();
+  const updateFormContent = async () => {
+    try {
+      const jsonElements = JSON.stringify(elements);
+      await UpdateFormContent(id, jsonElements);
+      toast({
+        title: "Success",
+        description: `Your form has been saved, ${elements} && ${jsonElements}`,
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+        variant: "destructive",
+      });
+    }
+  };
+  return (
+    <Button
+      variant={"outline"}
+      className="gap-2"
+      onClick={() => {
+        startTransition(updateFormContent);
+      }}
+    >
+      <HiSaveAs />
+      Save
+      {loading && <FaSpinner className="animate-spin" />}
+    </Button>
+  );
+}
+
+export default SaveFormBtn;
